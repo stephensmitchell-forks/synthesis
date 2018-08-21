907901da4a76b3fc8ffde8527fb0f1532cf5f9af
Nathan Walker <nathanwalker1208@gmail.com>
2018-08-21 08:37:00 -0700

    Added joint properties form

---
+++ exporters/field_exporter/FieldExporter/Components/JointPropertiesForm.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace FieldExporter.Components
+{
+    partial class JointPropertiesForm
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gamepieceGroupbox = new System.Windows.Forms.GroupBox();
+            this.gamepieceLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.centerLabel = new System.Windows.Forms.Label();
+            this.selectCenterButton = new System.Windows.Forms.Button();
+            this.jointCheckBox = new System.Windows.Forms.CheckBox();
+            this.axisLabel = new System.Windows.Forms.Label();
+            this.selectAxisButton = new System.Windows.Forms.Button();
+            this.gamepieceGroupbox.SuspendLayout();
+            this.gamepieceLayoutPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gamepieceGroupbox
+            // 
+            this.gamepieceGroupbox.AutoSize = true;
+            this.gamepieceGroupbox.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.gamepieceGroupbox.Controls.Add(this.gamepieceLayoutPanel);
+            this.gamepieceGroupbox.Controls.Add(this.jointCheckBox);
+            this.gamepieceGroupbox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.gamepieceGroupbox.Location = new System.Drawing.Point(0, 0);
+            this.gamepieceGroupbox.Name = "gamepieceGroupbox";
+            this.gamepieceGroupbox.Size = new System.Drawing.Size(250, 77);
+            this.gamepieceGroupbox.TabIndex = 1;
+            this.gamepieceGroupbox.TabStop = false;
+            // 
+            // gamepieceLayoutPanel
+            // 
+            this.gamepieceLayoutPanel.AutoSize = true;
+            this.gamepieceLayoutPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.gamepieceLayoutPanel.ColumnCount = 2;
+            this.gamepieceLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.gamepieceLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.gamepieceLayoutPanel.Controls.Add(this.selectAxisButton, 1, 1);
+            this.gamepieceLayoutPanel.Controls.Add(this.axisLabel, 0, 1);
+            this.gamepieceLayoutPanel.Controls.Add(this.centerLabel, 0, 0);
+            this.gamepieceLayoutPanel.Controls.Add(this.selectCenterButton, 1, 0);
+            this.gamepieceLayoutPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.gamepieceLayoutPanel.Location = new System.Drawing.Point(3, 16);
+            this.gamepieceLayoutPanel.Name = "gamepieceLayoutPanel";
+            this.gamepieceLayoutPanel.RowCount = 2;
+            this.gamepieceLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.gamepieceLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.gamepieceLayoutPanel.Size = new System.Drawing.Size(244, 58);
+            this.gamepieceLayoutPanel.TabIndex = 1;
+            // 
+            // centerLabel
+            // 
+            this.centerLabel.AutoSize = true;
+            this.centerLabel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.centerLabel.Location = new System.Drawing.Point(3, 3);
+            this.centerLabel.Margin = new System.Windows.Forms.Padding(3);
+            this.centerLabel.Name = "centerLabel";
+            this.centerLabel.Size = new System.Drawing.Size(107, 23);
+            this.centerLabel.TabIndex = 1;
+            this.centerLabel.Text = "Center: [0.0, 0.0, 0.0]";
+            this.centerLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // selectCenterButton
+            // 
+            this.selectCenterButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.selectCenterButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.selectCenterButton.Location = new System.Drawing.Point(179, 3);
+            this.selectCenterButton.Name = "selectCenterButton";
+            this.selectCenterButton.Size = new System.Drawing.Size(62, 23);
+            this.selectCenterButton.TabIndex = 0;
+            this.selectCenterButton.Text = "Select";
+            this.selectCenterButton.UseVisualStyleBackColor = true;
+            // 
+            // jointCheckBox
+            // 
+            this.jointCheckBox.AutoSize = true;
+            this.jointCheckBox.Checked = true;
+            this.jointCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.jointCheckBox.Location = new System.Drawing.Point(6, 0);
+            this.jointCheckBox.Name = "jointCheckBox";
+            this.jointCheckBox.Size = new System.Drawing.Size(60, 17);
+            this.jointCheckBox.TabIndex = 0;
+            this.jointCheckBox.Text = "Jointed";
+            this.jointCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // axisLabel
+            // 
+            this.axisLabel.AutoSize = true;
+            this.axisLabel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.axisLabel.Location = new System.Drawing.Point(3, 32);
+            this.axisLabel.Margin = new System.Windows.Forms.Padding(3);
+            this.axisLabel.Name = "axisLabel";
+            this.axisLabel.Size = new System.Drawing.Size(95, 23);
+            this.axisLabel.TabIndex = 4;
+            this.axisLabel.Text = "Axis: [0.0, 0.0, 0.0]";
+            this.axisLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // selectAxisButton
+            // 
+            this.selectAxisButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.selectAxisButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.selectAxisButton.Location = new System.Drawing.Point(179, 32);
+            this.selectAxisButton.Name = "selectAxisButton";
+            this.selectAxisButton.Size = new System.Drawing.Size(62, 23);
+            this.selectAxisButton.TabIndex = 5;
+            this.selectAxisButton.Text = "Select";
+            this.selectAxisButton.UseVisualStyleBackColor = true;
+            // 
+            // JointPropertiesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.Controls.Add(this.gamepieceGroupbox);
+            this.MinimumSize = new System.Drawing.Size(250, 0);
+            this.Name = "JointPropertiesForm";
+            this.Size = new System.Drawing.Size(250, 77);
+            this.gamepieceGroupbox.ResumeLayout(false);
+            this.gamepieceGroupbox.PerformLayout();
+            this.gamepieceLayoutPanel.ResumeLayout(false);
+            this.gamepieceLayoutPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gamepieceGroupbox;
+        private System.Windows.Forms.TableLayoutPanel gamepieceLayoutPanel;
+        private System.Windows.Forms.Label centerLabel;
+        private System.Windows.Forms.Button selectCenterButton;
+        private System.Windows.Forms.CheckBox jointCheckBox;
+        private System.Windows.Forms.Button selectAxisButton;
+        private System.Windows.Forms.Label axisLabel;
+    }
+}
---
+++ exporters/field_exporter/FieldExporter/Components/JointPropertiesForm.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Inventor;
+
+namespace FieldExporter.Components
+{
+    public partial class JointPropertiesForm : UserControl
+    {
+        private BXDVector3 _center = new BXDVector3(0, 0, 0);
+        public BXDVector3 Center
+        {
+            get => _center;
+            set
+            {
+                if (InvokeRequired)
+                {
+                    Invoke((Action<BXDVector3>)((BXDVector3 v) => Center = v), value);
+                    return;
+                }
+
+                _center = value;
+                centerLabel.Text = "Center: [" + value.x.ToString("N1") + ',' + value.y.ToString("N1") + ',' + value.z.ToString("N1") + ']';
+            }
+        }
+
+        private BXDVector3 _axis = new BXDVector3(1, 0, 0);
+        public BXDVector3 Axis
+        {
+            get => _axis;
+            set
+            {
+                if (InvokeRequired)
+                {
+                    Invoke((Action<BXDVector3>)((BXDVector3 v) => Axis = v), value);
+                    return;
+                }
+
+                _axis = value;
+                axisLabel.Text = "Axis: [" + value.x.ToString("N1") + ',' + value.y.ToString("N1") + ',' + value.z.ToString("N1") + ']';
+            }
+        }
+
+        public JointPropertiesForm()
+        {
+            InitializeComponent();
+        }
+
+        public bool IsJointed
+        {
+            get => jointCheckBox.Checked;
+            set => jointCheckBox.Checked = value;
+        }
+
+        /// <summary>
+        /// Returns the configured gamepiece settings, or null if not a gamepiece.
+        /// </summary>
+        /// <returns></returns>
+        public PropertySet.FieldJoint Joint
+        {
+            get
+            {
+                if (jointCheckBox.Checked)
+                    return new PropertySet.FieldJoint(Center, Axis);
+                else
+                    return null;
+            }
+            set
+            {
+                if (value == null)
+                    jointCheckBox.Checked = false;
+                else
+                {
+                    jointCheckBox.Checked = true;
+                    Center = value.Center;
+                    Axis = value.Axis;
+                }
+            }
+        }
+
+        InteractionEvents interactionEvents;
+        SelectEvents selectEvents;
+
+        private void interactionEvents_OnActivate()
+        {
+            selectEvents = interactionEvents.SelectEvents;
+            selectEvents.AddSelectionFilter(SelectionFilterEnum.kSketchPointFilter);
+            selectEvents.OnSelect += selectEvents_OnSelect;
+        }
+
+        bool selectingCenter = false;
+        bool selectingAxis = false;
+
+        private void selectEvents_OnSelect(ObjectsEnumerator justSelectedEntities, SelectionDeviceEnum selectionDevice, Inventor.Point modelPosition, Point2d viewPosition, Inventor.View view)
+        {
+            foreach (dynamic selectedEntity in justSelectedEntities)
+            {
+                if (selectingCenter && selectedEntity is SketchPoint point)
+                {
+                    Center = new BXDVector3(point.Geometry3d.X, point.Geometry3d.Y, point.Geometry3d.Z);
+                    DisableInteractionEvents();
+                    break;
+                }
+                else
+                {
+                    DisableInteractionEvents();
+                    break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Enables interaction events with Inventor.
+        /// </summary>
+        public void EnableInteractionEvents()
+        {
+            if (Program.INVENTOR_APPLICATION.ActiveDocument == Program.ASSEMBLY_DOCUMENT)
+            {
+                try
+                {
+                    interactionEvents = Program.INVENTOR_APPLICATION.CommandManager.CreateInteractionEvents();
+                    interactionEvents.OnActivate += interactionEvents_OnActivate;
+                    interactionEvents.Start();
+
+                    if (selectingCenter)
+                    {
+                        selectCenterButton.Text = "Cancel";
+                        selectAxisButton.Enabled = false;
+                    }
+                    else
+                    {
+                        selectAxisButton.Text = "Cancel";
+                        selectCenterButton.Enabled = false;
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Cannot enter select mode.", "Document not found.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Can only enter select mode for " + Program.ASSEMBLY_DOCUMENT.DisplayName);
+            }
+        }
+
+        /// <summary>
+        /// Disables interaction events with Inventor.
+        /// </summary>
+        public void DisableInteractionEvents()
+        {
+            interactionEvents.Stop();
+            selectingCenter = false;
+            selectingAxis = false;
+
+            if (InvokeRequired)
+            {
+                Invoke((Action)(() =>
+                {
+                    selectCenterButton.Text = "Select";
+                    selectAxisButton.Text = "Select";
+                    selectCenterButton.Enabled = true;
+                    selectAxisButton.Enabled = true;
+                }));
+            }
+            else
+            {
+                selectCenterButton.Text = "Select";
+                selectAxisButton.Text = "Select";
+                selectCenterButton.Enabled = true;
+                selectAxisButton.Enabled = true;
+            }
+        }
+
+        private void selectCenterButton_Click(object sender, EventArgs e)
+        {
+            if (!selectingAxis && !selectingCenter)
+            {
+                selectingCenter = true;
+                EnableInteractionEvents();
+            }
+            else
+            {
+                DisableInteractionEvents();
+            }
+        }
+
+        private void selectAxisButton_Click(object sender, EventArgs e)
+        {
+            if (!selectingAxis && !selectingCenter)
+            {
+                selectingAxis = true;
+                EnableInteractionEvents();
+            }
+            else
+            {
+                DisableInteractionEvents();
+            }
+        }
+
+        private void jointCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (jointCheckBox.Checked)
+            {
+                centerLabel.Enabled = true;
+                selectCenterButton.Enabled = true;
+                axisLabel.Enabled = true;
+                selectAxisButton.Enabled = true;
+            }
+            else
+            {
+                if (selectingCenter || selectingAxis)
+                    DisableInteractionEvents();
+
+                Center = new BXDVector3(0, 0, 0);
+                Axis = new BXDVector3(1, 0, 0);
+
+                centerLabel.Enabled = false;
+                selectCenterButton.Enabled = false;
+                axisLabel.Enabled = false;
+                selectAxisButton.Enabled = false;
+            }
+        }
+    }
+}
